a52a44f9e8a938eade368bc7ccb20fa1191a011c
sebinn <bhy401@naver.com>
2020-05-19 23:23:37 +0900

    여러가지 수정

--- app/src/main/java/com/example/android_elder/MainActivity.java
+++ app/src/main/java/com/example/android_elder/MainActivity.java
@@ -1,32 +1,71 @@
 package com.example.android_elder;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
+import android.app.Activity;
 import android.app.TabActivity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends TabActivity {
+
+
+    // 탭 호스트 변수 선언
+    TabHost tabHost;
+    ImageView tabwg1;
+    ImageView tabwg2;
+    ImageView tabwg3;
+
+    // 이미지 버튼 변수 (메뉴)
+    // 탭1
+
+    //경로우대
+    ImageButton btn_cane;
+    //교통약지 이동지원
+    ImageButton btn_traf;
+    //노인장애인 보호구역
+    ImageButton btn_protect;
+    //병원
+    ImageButton btn_hosp;
+
+    //탭2
+    //경로당
+    ImageButton btn_elder;
+    //약수터
+    ImageButton btn_mineral;
+    //실외운동기구
+    ImageButton btn_health;
+    //복지센터
+    ImageButton btn_welfare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TabHost tabHost = getTabHost();
+        tabHost = getTabHost();
         tabHost.getTabWidget().setDividerDrawable(null); // 탭라인 제거
 
-        ImageView tabwg1 = new ImageView(this);
+        tabwg1 = new ImageView(this);
         tabwg1.setImageResource(R.drawable.tab_01);
 
-        ImageView tabwg2 = new ImageView(this);
+        tabwg2 = new ImageView(this);
         tabwg2.setImageResource(R.drawable.tab_02);
 
-        ImageView tabwg3 = new ImageView(this);
+        tabwg3 = new ImageView(this);
         tabwg3.setImageResource(R.drawable.tab_03);
 
         TabHost.TabSpec tabSpecTab1 = tabHost.newTabSpec("TAB1").setIndicator(tabwg1);
@@ -81,5 +120,60 @@ public class MainActivity extends TabActivity {
         tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = (screenHeight * 15) / 200;
         tabHost.getTabWidget().getChildAt(1).getLayoutParams().height = (screenHeight * 15) / 200;
         tabHost.getTabWidget().getChildAt(2).getLayoutParams().height = (screenHeight * 15) / 200;
+
+
+        // 메뉴 이동
+        // 경로우대
+        btn_cane = (ImageButton)findViewById(R.id.cane);
+        btn_cane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(), Path_old.class);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
+//public class MainActivity extends Activity {
+//    Spinner s;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_path);
+//
+//        final RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.list_layout);
+//        final LinearLayout linearlayout = (LinearLayout)findViewById(R.id.path_map);
+//
+////        final ListView listview = (ListView)findViewById(R.id.path_list);
+////        final TextView textview = (TextView)findViewById(R.id.hiroo);
+//
+//        s = (Spinner)findViewById(R.id.search_spin);
+//        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.search_list, android.R.layout.simple_spinner_dropdown_item);
+//        s.setAdapter(adapter);
+//
+//        relativeLayout.setVisibility(View.VISIBLE);
+//        linearlayout.setVisibility(View.INVISIBLE);
+//
+//        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+////                Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+//                if(i==0){
+//                    Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+//                    relativeLayout.setVisibility(View.VISIBLE);
+//                    linearlayout.setVisibility(View.INVISIBLE);
+//                } else if(i==1){
+//                    Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+//                    relativeLayout.setVisibility(View.INVISIBLE);
+//                    linearlayout.setVisibility(View.VISIBLE);
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//                relativeLayout.setVisibility(View.VISIBLE);
+//            }
+//        });
+//    }
+//}
---
+++ app/src/main/java/com/example/android_elder/Path_old.java
@@ -0,0 +1,126 @@
+package com.example.android_elder;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+//public class Path_old extends AppCompatActivity implements OnMapReadyCallback {
+//
+//    private GoogleMap gMap;
+//    MapFragment mapFrag;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.path_map);
+//        mapFrag.getMapAsync(this);
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//
+//        gMap = map;
+//        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+//        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 15));
+////        mMap = googleMap;
+////
+////        LatLng SEOUL = new LatLng(37.56, 126.97);
+////
+////        MarkerOptions markerOptions = new MarkerOptions();
+////        markerOptions.position(SEOUL);
+////        markerOptions.title("서울");
+////        markerOptions.snippet("한국의 수도");
+////        mMap.addMarker(markerOptions);
+////
+////        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+////        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+//    }
+//
+//}
+
+public class Path_old extends AppCompatActivity implements OnMapReadyCallback {
+    Spinner spinner;
+    RelativeLayout relativeLayout;
+    LinearLayout linearlayout;
+    GoogleMap gMap;
+    MapFragment mapFrag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_path);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.path_map);
+        mapFragment.getMapAsync(this);
+
+//        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.path_map);
+//        mapFrag.getMapAsync(this);
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.list_layout);
+        linearlayout = (LinearLayout)findViewById(R.id.path_map_layout);
+
+        spinner = (Spinner)findViewById(R.id.search_spin);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.search_list, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        relativeLayout.setVisibility(View.VISIBLE);
+        linearlayout.setVisibility(View.INVISIBLE);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+                if(i==0){
+                    Toast.makeText(Path_old.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+                    relativeLayout.setVisibility(View.VISIBLE);
+                    linearlayout.setVisibility(View.INVISIBLE);
+                } else if(i==1){
+                    Toast.makeText(Path_old.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+                    relativeLayout.setVisibility(View.INVISIBLE);
+                    linearlayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                relativeLayout.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void onMapReady(GoogleMap map) {
+
+//        gMap = map;
+//        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+//        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 15));
+
+        gMap = map;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        gMap.addMarker(markerOptions);
+
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
+}
