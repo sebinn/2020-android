e8bb79e80b121d4b1ec010aa0a6978ffb8ecfa3b
sebinn <bhy401@naver.com>
2020-06-04 23:02:13 +0900

    첫번째 탭호스트 병원빼고 디비저장

--- app/src/main/java/com/example/androidelder/Protect_elder.java
+++ app/src/main/java/com/example/androidelder/Protect_elder.java
@@ -1,10 +1,17 @@
 package com.example.androidelder;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -16,6 +23,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
 public class Protect_elder extends AppCompatActivity implements OnMapReadyCallback {
     Spinner spinner;
     RelativeLayout relativeLayout;
@@ -23,6 +33,28 @@ public class Protect_elder extends AppCompatActivity implements OnMapReadyCallba
     LinearLayout linearlayout;
     GoogleMap gMap;
     SupportMapFragment mapFragment;
+
+    // 디비
+    myDBHelper myDBHelper;
+    SQLiteDatabase sqlDB;
+
+    // 전체 리스트
+    ArrayList<String> arrayList1;
+    ArrayAdapter<String> arrayAdapter1;
+    ListView listView1;
+
+    // 검색 리스트
+    ArrayList<String> arrayList2;
+    ArrayAdapter<String> arrayAdapter2;
+    ListView listView2;
+
+    // 검색 아이콘
+    ImageButton imgbtn;
+
+    // 검색창
+    EditText editText;
+    String text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,20 +80,79 @@ public class Protect_elder extends AppCompatActivity implements OnMapReadyCallba
         relativeLayout2.setVisibility(View.INVISIBLE);
         linearlayout.setVisibility(View.INVISIBLE);
 
+        //디비관련
+        myDBHelper = new myDBHelper(this);
+        sqlDB = myDBHelper.getReadableDatabase();
+        final Cursor cursor;
+        cursor = sqlDB.rawQuery("SELECT * From Protect_elder;", null);
+        cursor.moveToFirst();
+
+        int count = cursor.getCount();
+        listView1 = (ListView)findViewById(R.id.prot_list_all);
+        arrayList1 = new ArrayList<String>();
+        arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList1);
+        for(int k=0; k<count; k++){
+            arrayList1.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        sqlDB.close();
+
+        //이미지 버튼
+        imgbtn = (ImageButton)findViewById(R.id.search_icon_prot);
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText = (EditText)findViewById(R.id.search_prot);
+                text = editText.getText().toString();
+                Toast.makeText(view.getContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 //                Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                 if(i==0){
+                    listView1.setAdapter(arrayAdapter1);
                     Toast.makeText(Protect_elder.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.VISIBLE);
                     relativeLayout2.setVisibility(View.INVISIBLE);
                     linearlayout.setVisibility(View.INVISIBLE);
                 } else if(i==1){
-                    Toast.makeText(Protect_elder.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.INVISIBLE);
                     relativeLayout2.setVisibility(View.VISIBLE);
                     linearlayout.setVisibility(View.INVISIBLE);
+                    imgbtn = (ImageButton)findViewById(R.id.search_icon_prot);
+                    imgbtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            editText = (EditText)findViewById(R.id.search_prot);
+                            text = editText.getText().toString();
+                            if(text.length() > 0){
+
+                                sqlDB = myDBHelper.getReadableDatabase();
+                                final Cursor cursor;
+                                cursor = sqlDB.rawQuery("SELECT * From Protect_elder WHERE name LIKE '%" + text + "%'", null);
+                                cursor.moveToFirst();
+
+                                int count = cursor.getCount();
+                                listView2 = (ListView)findViewById(R.id.prot_list);
+                                arrayList2 = new ArrayList<String>();
+                                arrayAdapter2 = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, arrayList2);
+
+                                for(int k=0; k<count; k++){
+                                    arrayList2.add(cursor.getString(0));
+                                    cursor.moveToNext();
+                                }
+                                cursor.close();
+                                sqlDB.close();
+
+                                listView2.setAdapter(arrayAdapter2);
+                            }
+
+                        }
+                    });
                 } else if(i==2){
                     Toast.makeText(Protect_elder.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.INVISIBLE);
@@ -95,5 +186,19 @@ public class Protect_elder extends AppCompatActivity implements OnMapReadyCallba
 
         gMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         gMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context){
+            super(context, "Elder.db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
     }
 }
--- app/src/main/java/com/example/androidelder/Traffic.java
+++ app/src/main/java/com/example/androidelder/Traffic.java
@@ -1,10 +1,17 @@
 package com.example.androidelder;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -12,46 +19,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-//public class Path_old extends AppCompatActivity implements OnMapReadyCallback {
-//
-//    private GoogleMap gMap;
-//    MapFragment mapFrag;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.path_map);
-//        mapFrag.getMapAsync(this);
-//    }
-//
-//    @Override
-//    public void onMapReady(GoogleMap map) {
-//
-//        gMap = map;
-//        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-//        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 15));
-////        mMap = googleMap;
-////
-////        LatLng SEOUL = new LatLng(37.56, 126.97);
-////
-////        MarkerOptions markerOptions = new MarkerOptions();
-////        markerOptions.position(SEOUL);
-////        markerOptions.title("서울");
-////        markerOptions.snippet("한국의 수도");
-////        mMap.addMarker(markerOptions);
-////
-////        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-////        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-//    }
-//
-//}
+import java.util.ArrayList;
 
 public class Traffic extends AppCompatActivity implements OnMapReadyCallback {
     Spinner spinner;
@@ -60,6 +33,27 @@ public class Traffic extends AppCompatActivity implements OnMapReadyCallback {
     LinearLayout linearlayout;
     GoogleMap gMap;
     SupportMapFragment mapFragment;
+
+    // 디비
+    myDBHelper myDBHelper;
+    SQLiteDatabase sqlDB;
+
+    // 전체 리스트
+    ArrayList<String> arrayList1;
+    ArrayAdapter<String> arrayAdapter1;
+    ListView listView1;
+
+    // 검색 리스트
+    ArrayList<String> arrayList2;
+    ArrayAdapter<String> arrayAdapter2;
+    ListView listView2;
+
+    // 검색 아이콘
+    ImageButton imgbtn;
+
+    // 검색창
+    EditText editText;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,20 +79,78 @@ public class Traffic extends AppCompatActivity implements OnMapReadyCallback {
         relativeLayout2.setVisibility(View.INVISIBLE);
         linearlayout.setVisibility(View.INVISIBLE);
 
+        //디비관련
+        myDBHelper = new myDBHelper(this);
+        sqlDB = myDBHelper.getReadableDatabase();
+        final Cursor cursor;
+        cursor = sqlDB.rawQuery("SELECT * From Vehicle_support;", null);
+        cursor.moveToFirst();
+
+        int count = cursor.getCount();
+        listView1 = (ListView)findViewById(R.id.traff_list_all);
+        arrayList1 = new ArrayList<String>();
+        arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList1);
+        for(int k=0; k<count; k++){
+            arrayList1.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        sqlDB.close();
+
+        //이미지 버튼
+        imgbtn = (ImageButton)findViewById(R.id.search_icon_traff);
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText = (EditText)findViewById(R.id.search_traff);
+                text = editText.getText().toString();
+                Toast.makeText(view.getContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(MainActivity.this,"선택된 아이템 : "+ s.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                 if(i==0){
+                    listView1.setAdapter(arrayAdapter1);
                     Toast.makeText(Traffic.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.VISIBLE);
                     relativeLayout2.setVisibility(View.INVISIBLE);
                     linearlayout.setVisibility(View.INVISIBLE);
                 } else if(i==1){
-                    Toast.makeText(Traffic.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.INVISIBLE);
                     relativeLayout2.setVisibility(View.VISIBLE);
                     linearlayout.setVisibility(View.INVISIBLE);
+                    imgbtn = (ImageButton)findViewById(R.id.search_icon_traff);
+                    imgbtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            editText = (EditText)findViewById(R.id.search_traff);
+                            text = editText.getText().toString();
+                            if(text.length() > 0){
+
+                                sqlDB = myDBHelper.getReadableDatabase();
+                                final Cursor cursor;
+                                cursor = sqlDB.rawQuery("SELECT * From Vehicle_support WHERE name LIKE '%" + text + "%'", null);
+                                cursor.moveToFirst();
+
+                                int count = cursor.getCount();
+                                listView2 = (ListView)findViewById(R.id.traff_list);
+                                arrayList2 = new ArrayList<String>();
+                                arrayAdapter2 = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, arrayList2);
+
+                                for(int k=0; k<count; k++){
+                                    arrayList2.add(cursor.getString(0));
+                                    cursor.moveToNext();
+                                }
+                                cursor.close();
+                                sqlDB.close();
+
+                                listView2.setAdapter(arrayAdapter2);
+                            }
+
+                        }
+                    });
                 } else if(i==2){
                     Toast.makeText(Traffic.this,"선택된 아이템 : "+ spinner.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
                     relativeLayout.setVisibility(View.INVISIBLE);
@@ -132,5 +184,19 @@ public class Traffic extends AppCompatActivity implements OnMapReadyCallback {
 
         gMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         gMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context){
+            super(context, "Elder.db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
     }
 }
